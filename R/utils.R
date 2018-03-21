5bf607edccd723e8a854323031d0195a6e270767
Mark Andrews <mjandrews.net@gmail.com>
2018-03-21 19:21:46 +0000

    Add new function to utils: aggregate
    This is a helper function for use with the dplyr

--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,35 @@
 ilogit <- function(log_odds){
   1/(1 + exp(-log_odds))
 }
+
+#' Apply function over selected columns
+#' 
+#' Select columns using `dplyr`'s `select` function, 
+#' and then apply a function to each row. The function
+#' default to `sum`. 
+#' @param selection Use a `select` helper, e.g. `starts_with()` to selection variables.
+#' @param fun A function to apply to each row of selected columns.
+#' @author Mark Andrews (mark.andrews@@ntu.ac.uk)
+#' @export aggregate
+#' @examples
+#' set.seed(42)
+#' N <- 10
+#' Df <- tibble(p_1 = rnorm(N),
+#'              p_2 = rnorm(N),
+#'              q_1 = rnorm(N),
+#'              q_2 = rnorm(N))
+#' Df
+#' 
+#' # mean over p_1 and p_2
+#' aggregate(Df, starts_with('p'), mean)
+#' 
+#' # max over p_1 and q_1
+#' aggregate(Df, ends_with('1'), max)
+#' 
+#' # sum over p_1, p_2, q_1, q_2
+#' aggregate(Df, everything())
+aggregate <- function(Df, selection, fun=sum){
+   select(Df, selection) %>% apply(1, fun)
+}
+
+
